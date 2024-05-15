61f0815070e32465156ca1dda5731a4928370115
dstrn <dstrn825@gmail.com>
2024-05-15 14:47:29 +0200

    fix

--- server/src/main/java/com/cardgame/cardgame/actor/controller/ActorRepo.java
+++ server/src/main/java/com/cardgame/cardgame/actor/controller/ActorRepo.java
@@ -1,5 +1,14 @@
 package com.cardgame.cardgame.actor.controller;
 
 import com.cardgame.cardgame.actor.model.Actor;
-import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ActorRepo extends CrudRepository<Actor, Integer> {
+    List<Actor> findAll();
+
+}
