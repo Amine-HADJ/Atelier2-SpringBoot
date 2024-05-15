b0841330c570a28d390682862fe729a87ebf3e4a
Amine-HADJ <mohammed.hadj-hamdri@cpe.fr>
2024-05-15 15:26:36 +0200

    MarketController

--- server/src/main/java/com/cardgame/cardgame/controllers/MarketController.java
+++ server/src/main/java/com/cardgame/cardgame/controllers/MarketController.java
@@ -1,10 +1,14 @@
 package com.cardgame.cardgame.controllers;
 
 import com.cardgame.cardgame.models.User;
+import com.cardgame.cardgame.models.Card;
 import com.cardgame.cardgame.services.MarketService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class MarketController {
@@ -15,8 +19,19 @@ public class MarketController {
         this.mService = mService;
     }
 
-    @PostMapping("/signup")
-    public void inscription() {
-        return;
+    @PostMapping("/buycard")
+    public void buyCard(@RequestParam Integer userId, @RequestParam Card card) {
+        mService.buyCard(userId, card);
     }
+
+
+    @PostMapping("/sellcard")
+    public void sellCard(@RequestParam Integer userId, @RequestParam Card card) {
+        mService.sellCard(userId, card);
+    }
+
+   
+
+    
+    
 }
