f8e3f2a371b880d8f767a8c8d6bf571f1fd1f3a2
Amine-HADJ <mohammed.hadj-hamdri@cpe.fr>
2024-05-15 15:18:55 +0200

    MarketService ok

--- server/src/main/java/com/cardgame/cardgame/services/MarketService.java
+++ server/src/main/java/com/cardgame/cardgame/services/MarketService.java
@@ -6,6 +6,7 @@ import com.cardgame.cardgame.models.Card;
 import com.cardgame.cardgame.models.User;
 import com.cardgame.cardgame.repositories.CardRepo;
 import com.cardgame.cardgame.repositories.InventoryRepo;
+import com.cardgame.cardgame.repositories.UserRepo;
 
 
 @Service
@@ -13,25 +14,68 @@ public class MarketService {
 
     private CardRepo cardRepo;
     private InventoryRepo inventoryRepo;
+    private UserRepo userRepo;
 
 
-    // TODO: Implement MarketService
-    // - buyCard
-    // - sellCard
-    // - InventoryUpdate
-    // - CardRepoUpdate
 
     public void buyCard(Integer userId, Card card) {
-        
-    }
 
+        //Check if the card is available
+        if (cardRepo.findById(card.getId()).isEmpty()){
+            System.out.println("Card not available");
+            return;
+        }
+
+
+        
+        //Check if the user has enough money
+        if (userRepo.findById(userId).get().getWallet() < card.getPrice()){
+            System.out.println("Not enough money");
+            return;
+        }
+
+        //Add card to user inventory
+        inventoryRepo.findById(userId).get().addCard(card);
+
+        //Update user wallet
+        userRepo.findById(userId).get().setWallet( - (double) card.getPrice() );
+
+        //Remove card from cardRepo
+        cardRepo.deleteById(card.getId());
+
+        }
+
+
+        public void sellCard(Integer userId, Card card) {
+
+            //Check if the card is in the user inventory
+            if (inventoryRepo.findById(userId).get().getCards().contains(card)){
+                System.out.println("Card not in inventory");
+                return;
+            }
+
+            //Add card to cardRepo
+            cardRepo.save(card);
+
+            //Update user wallet
+            userRepo.findById(userId).get().setWallet( (double) card.getPrice() );
+
+            //Remove card from user inventory
+            inventoryRepo.findById(userId).get().getCards().remove(card);
+
+        }
+
+
+
+   
+            
 
     }
 
 
     
 
+    
 
 
 
-}
