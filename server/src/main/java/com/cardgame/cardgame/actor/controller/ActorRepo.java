449fc48f43af411026976c7f414da94885e76137
Amine-HADJ <mohammed.hadj-hamdri@cpe.fr>
2024-05-15 14:45:00 +0200

    CardService ok

--- server/src/main/java/com/cardgame/cardgame/actor/controller/ActorRepo.java
+++ server/src/main/java/com/cardgame/cardgame/actor/controller/ActorRepo.java
@@ -3,5 +3,3 @@ package com.cardgame.cardgame.actor.controller;
 import com.cardgame.cardgame.actor.model.Actor;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ActorRepo extends CrudRepository<Actor,Integer> {
-}
--- server/src/main/java/com/cardgame/cardgame/services/CardService.java
+++ server/src/main/java/com/cardgame/cardgame/services/CardService.java
@@ -1,5 +1,31 @@
 package com.cardgame.cardgame.services;
 
+import com.cardgame.cardgame.repositories.CardRepo;
+import com.cardgame.cardgame.models.Card;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class CardService {
 
+    private CardRepo CardRepo;
+
+
+
+   // Card service generate 5 randoms cards from CardRepository
+    public List<Card> generateCards() {
+        List<Card> cards = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            //getting length of CardRepo
+            int length = (int) CardRepo.count();
+            cards.add(CardRepo.findById((int) (Math.random() * length) + 1).get());
+        }
+        return cards;
+    }
 }
+
+
+    
