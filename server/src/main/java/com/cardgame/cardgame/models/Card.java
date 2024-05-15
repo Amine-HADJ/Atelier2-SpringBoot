fe48bf6592ff12ca29382088ed9a868626c52aa1
dstrn <dstrn825@gmail.com>
2024-05-15 14:14:15 +0200

    user and card

--- server/src/main/java/com/cardgame/cardgame/models/Card.java
+++ server/src/main/java/com/cardgame/cardgame/models/Card.java
@@ -1,5 +1,108 @@
 package com.cardgame.cardgame.models;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
+@Entity
 public class Card {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String familyName;
+    private String imgSrc;
+    private String name;
+    private String description;
+    private int hp;
+    private int energy;
+    private int attack;
+    private int defense;
+    private double price;
 
+    public Card(String familyName, String imgSrc, String name, String description, int hp, int energy, int attack, int defense, double price) {
+        this.familyName = familyName;
+        this.imgSrc = imgSrc;
+        this.name = name;
+        this.description = description;
+        this.hp = hp;
+        this.energy = energy;
+        this.attack = attack;
+        this.defense = defense;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public String getImgSrc() {
+        return imgSrc;
+    }
+
+    public void setImgSrc(String imgSrc) {
+        this.imgSrc = imgSrc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- server/src/main/java/com/cardgame/cardgame/models/User.java
+++ server/src/main/java/com/cardgame/cardgame/models/User.java
@@ -1,5 +1,59 @@
 package com.cardgame.cardgame.models;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
+    private double wallet;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.wallet = 0.0;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(double wallet) {
+        this.wallet = wallet;
+    }
 }
